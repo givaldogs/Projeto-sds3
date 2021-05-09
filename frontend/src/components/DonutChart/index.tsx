e02ef3a98f516d59337b018f64905a0b73e7b0ee
givaldogs <givaldowanda@gmail.com>
2021-05-09 02:52:14 -0300

    corredo build netlify

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -19,18 +19,9 @@ const DonutChart = () => {
                 const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
-                // chartData = { labels: myLabels, series: mySeries};
                 setChartData({ labels: myLabels, series: mySeries });
-                console.log(chartData)
             });
-
     }, []);
-
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
