0acbccbeec25524ae4c86f0a7ac1e5f26dbc82f6
givaldogs <givaldowanda@gmail.com>
2021-05-09 13:04:36 -0300

    retiraro varialve ROUTER

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,8 @@
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+//import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 
 const Routes = () => {
     return (
